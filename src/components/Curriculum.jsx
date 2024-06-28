52f2c37a360ab4eb2f1176fbe55943c3b45f0f3a
deauthe <deauth@ANURAGs-MacBook-Air.local>
2024-06-29 01:42:18 +0530

    added placeholder content

---
+++ src/components/Curriculum.jsx
@@ -0,0 +1,34 @@
+export default function Curriculum() {
+  return (
+    <section className="grid grid-cols-3 gap-8 m-6 w-5/6 mx-auto text-justify">
+      <div>
+        <h3 className="font-semibold">OUR EDUCATION</h3>
+        <p>
+          Lorem ipsum dolor consectetursit amet, consectetur adipiscing elit
+          consectetur euismod Lorem ipsum dolor sit amet, ea eum labitur scsstie
+          percipitoleat fabulas complectitur deterruisset at pro. Odio quaeque
+          reformidans est eu, expetendis intellegebat has ut, viderer invenire
+          ut his. Has molestie percipit an. Falli volumus efficiantur sed id, ad
+          vel noster propriae. Ius ut etiam vivendo, graeci iudicabit constituto
+          at mea.
+        </p>
+      </div>
+      <div>
+        <h3 className="font-semibold">Courses</h3>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet consectetur.</li>
+          <li>Lorem ipsum dolor sit.</li>
+        </ul>
+      </div>
+      <div>
+        <h3 className="font-semibold">Latest News</h3>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet consectetur.</li>
+          <li>Lorem ipsum dolor sit.</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
