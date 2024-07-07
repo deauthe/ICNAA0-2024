20db5d619812328dfd9a6de72dea85f1bb8bb9a2
akkshitgupta <96991785+akkshitgupta@users.noreply.github.com>
2024-07-07 18:52:54 +0530

    fix: scroll to view in navbar

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-import { Footer, Navbar } from "./components";
+import { Footer, Navbar, ScrollToHashElement } from "./components";
 
 export default function Layout() {
   return (
     <>
+      <ScrollToHashElement />
       <Navbar />
       <Outlet />
       <Footer />
